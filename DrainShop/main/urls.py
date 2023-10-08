f83d76977f5acf0741612e0fbcc76d8778edf8d3
TimurWASD <timur-zagret2004@mail.ru>
2023-10-08 19:08:55 +0600

    Создал админ панель, убрал темплейт добавления в бд

--- DrainShop/main/urls.py
+++ DrainShop/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('order/', views.order, name="order"),
-    path('payment/', views.payment, name="payment")
+    path('payment/', views.payment, name="payment"),
+    # path('add_category/', views.add_category, name="add_category")
 ]
--- DrainShop/main/views.py
+++ DrainShop/main/views.py
@@ -14,15 +14,15 @@ def order(requests):
 def payment(request):
     return render(request, 'main/payment.html')
 
-def add_category(request):
-    if request.method == "POST":
-        form = CategoryForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-    else:
-        form = CategoryForm()
-    return render(request, 'add_category.html', {'form': form})
-
-def show_categories(request):
-    cats = Category.objects.all()
-    return render(request, 'main/add_category.html', context={'cats': cats})
+# def add_category(request):
+#     if request.method == "POST":
+#         form = CategoryForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#     else:
+#         form = CategoryForm()
+#     return render(request, 'main/add_category.html', {'form': form})
+#
+# def show_categories(request):
+#     cats = Category.objects.all()
+#     return render(request, 'main/add_category.html', context={'cats': cats})
