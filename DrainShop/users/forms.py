3d164d4f346b2b082fb7c53ff0c018c3cb015e6b
TimurWASD <timur-zagret2004@mail.ru>
2023-11-12 20:02:37 +0600

    сделал регистрацию и виджеты

--- DrainShop/users/forms.py
+++ DrainShop/users/forms.py
@@ -8,8 +8,22 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email')
 
+        widgest = {
+            'username': forms.TextInput(attrs={
+                'class': 'register',
+                'placeholder': 'Введите login'
+            }
+            ),
+            'email': forms.TextInput(attrs={
+                'class': 'email',
+                'placeholder': 'Введите email'
+            }
+            )
+        }
+
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
