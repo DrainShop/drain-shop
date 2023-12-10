31e292c58c75a8dca9f92ece770a60377e801161
ShaysqTimur <timur.15.11.05@gmail.com>
2023-12-10 18:21:29 +0600

    исправление

--- DrainShop/users/urls.py
+++ DrainShop/users/urls.py
@@ -6,11 +6,4 @@ from users.forms import UserLoginForm
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path(
-        'login/',views.LoginView.as_view(
-            template_name="login.html",
-            authentication_form=UserLoginForm
-            ),
-        name='login'
-)
 ]
