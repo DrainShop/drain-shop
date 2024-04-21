d4ddf1b032c6873fbe3d3623f2305bc885298ae3
TimurWASD <timur-zagret2004@mail.ru>
2024-04-21 11:38:40 +0600

    wtf!?

--- DrainShop/DrainShop/urls.py
+++ DrainShop/DrainShop/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.urls import path
 from .yasg import urlpatterns as doc_url
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 
 
@@ -17,10 +17,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('api/v1/', include('api.urls')),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += doc_url
