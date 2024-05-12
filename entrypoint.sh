9c039f3ea8ce20c0dee973ff6c2bcdf16635caf4
dyussenov <dyusenov.aset.arm@gmail.com>
2024-05-12 23:29:18 +0500

    docker

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cmd="$@"
+cd DrainShop
+pwd
+echo "waiting for db"
+python3 -m manage collectstatic --noinput
+python3 -m manage migrate
+gunicorn core.wsgi:application --workers 2 --threads 4 --log-level debug --reload -b 0.0.0.0:8000
+exec $cmd
