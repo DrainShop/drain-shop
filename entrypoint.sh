8021c8337798b3620f21d9669fa6548f66fe1033
dyussenov <dyusenov.aset.arm@gmail.com>
2024-05-13 00:06:08 +0500

    gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ pwd
 echo "waiting for db"
 python3 -m manage collectstatic --noinput
 python3 -m manage migrate
-#gunicorn core.wsgi:application --workers 2 --threads 4 --log-level debug --reload -b 0.0.0.0:8000
+gunicorn DrainShop.wsgi:application --workers 2 --threads 4 --log-level debug --reload -b 0.0.0.0:8000
 python3 -m manage runserver
 exec $cmd
