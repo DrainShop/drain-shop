bfa12e832879c086988ec9bb977357e447150c34
dyussenov <dyusenov.aset.arm@gmail.com>
2024-05-20 21:33:36 +0500

    createsuperuser on docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ pwd
 echo "waiting for db"
 python3 -m manage collectstatic --noinput
 python3 -m manage migrate
+python3 -m manage createsuperuser --noinput
 gunicorn DrainShop.wsgi:application --workers 2 --threads 4 --log-level debug --reload -b 0.0.0.0:8000
 exec $cmd
